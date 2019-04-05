8801f954b71245861b60ce43a708b330f51e5705
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-04-05 14:06:31 +0300

    Changed the text of the app.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello there!
+            Changed text
           </a>
         </header>
       </div>
